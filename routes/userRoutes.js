e6fdabc23248a0696ac4b9b76fdc1f0f88e3e57c
Gisyo <civamagar@outlook.com>
2021-05-27 12:37:38 +0545

    first commit

---
+++ routes/userRoutes.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const userController = require('./../controllers/userController');
+const authController = require('./../controllers/authController');
+
+const router = express.Router();
+
+router.post('/signup', authController.signup);
+router.post('/login', authController.login);
+
+router.post('/forgotPassword', authController.forgotPassword);
+router.patch('/resetPassword/:token', authController.resetPassword);
+
+// Protect all routes after this middleware
+router.use(authController.protect);
+
+router.get('/logout', authController.logout);
+router.patch('/updateMyPassword', authController.updatePassword);
+router.get('/profile', userController.getMe, userController.getUser);
+router.delete('/deleteMe', userController.deleteMe);
+
+router.use(authController.restrictTo('admin'));
+
+router.route('/').post(userController.createUser);
+
+router
+  .route('/:id')
+  .get(userController.getUser)
+  .patch(userController.updateUser)
+  .delete(userController.deleteUser);
+
+module.exports = router;
